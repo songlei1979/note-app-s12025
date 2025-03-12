24c44d079eadb6047b0e34ef39bf4fd798a7a108
Lei <gabriel_sl19798@hotmail.com>
2025-03-13 11:24:15 +1300

    Route, home page link and register completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./components/Home";
+import Register from "./components/Register";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/register" element={<Register/>} />
+                        {/*<Route index element={<Home/>}/>*/}
+                        {/*<Route path="blogs" element={<Blogs/>}/>*/}
+                        {/*<Route path="contact" element={<Contact/>}/>*/}
+                        {/*<Route path="*" element={<NoPage/>}/>*/}
+
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
